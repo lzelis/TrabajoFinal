1ed575cb7d8b39d7ec56da8061adf1b9a08544d6
Leoyancsura <95661525+Leoyancsura@users.noreply.github.com>
2022-07-05 18:05:58 -0300

    Add files via upload

---
+++ AppCoder/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
+
+# Create your models here.
+
+
+class Posts(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField('titulo', max_length=40, blank = False, null = False )
+    subtitulo = models.CharField(max_length=40)
+    cuerpo = RichTextField()
+    autor = models.CharField(max_length=40)
+    fecha = models.DateField(default=datetime.now)
+    imagen = models.URLField(max_length = 255, blank = False, null= False)
+
+    class Meta:
+        verbose_name_plural = "Posts"
+        verbose_name = "Post"
+
+    def __str__(self):
+        return self.titulo
+
+class Autores(models.Model):
+    nombre = models.CharField(max_length=40)  
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    imagen = models.URLField(max_length = 255, blank = False, null= False)
+    
+    class Meta:
+        verbose_name_plural = "Autores"
+        verbose_name = "Autor"
+
+    def __str__(self): 
+        return f"{self.nombre}  {self.apellido} - {self.email}"  
+
+
+
+class Perfil(models.Model):
+    nombre = models.CharField(max_length=40)  
+    apellido = models.CharField(max_length=40)
+    email = models.EmailField()
+    
+    
+    class Meta:
+        verbose_name_plural = "Perfiles"
+        verbose_name = "Perfil"
+
+    def __str__(self): 
+        return f"{self.nombre}  {self.apellido} - {self.email}"
+
+class Avatar(models.Model):
+    #vinculo con el usuario
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #SUBCARPETA Avatares de media
+    imagen = models.ImageField(upload_to = 'avatares', null = True, blank = True)
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TrabajoFinal
+El siguiente blog fue realizado utilizando el framework de Django como proyecto final del curso de Python en CoderHouse.
+
+Autores: Leopoldo Zelis, 
+
+Instrucciones de uso:
+Clonar el proyecto.
+Crear las migraciones ejecutando 
+python manage.py makemigrations y 
+luego python manage.py migrate.
+Iniciar la aplicación con python manage.py runserver.
+Una vez inicializada la aplicación, al colocar en la barra del navegador http://127.0.0.1:8000 
+podrás visitar la url de Inicio y navegar dentro de las distintas páginas disponibles actualmente.
